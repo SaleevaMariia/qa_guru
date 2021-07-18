da1a1224450be0be6dbe8fb90fb40a16f705dbab
SaleevaMariia <sunlimen13@gmail.com>
2021-07-18 15:12:31 +0300

    add homework for Selenide lesson

---
+++ src/test/java/guru/qa/lesson_selenide/homework/task2/SelenideSoftAssertionsTest.java
@@ -0,0 +1,27 @@
+package guru.qa.lesson_selenide.homework.task2;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideSoftAssertionsTest {
+
+    @BeforeAll
+    public static void setUp(){
+        Configuration.baseUrl="https://github.com";
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    public void JUNIT5ExampleInSoftAssertionsExist() {
+        open("/selenide/selenide");
+        $("[data-content='Wiki']").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class"));
+    }
+}
---
+++ src/test/java/guru/qa/lesson_selenide/homework/task3/DragAndDropTest.java
@@ -0,0 +1,25 @@
+package guru.qa.lesson_selenide.homework.task3;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDropTest {
+
+    @BeforeAll
+    public static void setUp(){
+        Configuration.baseUrl="https://the-internet.herokuapp.com";
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    public void DragAndDropTest() {
+        open("/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(Condition.text("B"));
+    }
+}
